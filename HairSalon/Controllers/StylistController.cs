1ee9193f48d7209156a97732a5a905cd4f8eb3ee
Erik Z <molagg92@gmail.com>
2023-07-29 11:02:32 -0700

    fill out the controller and view for the Home page

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -1,11 +1,24 @@
-using System.Collections.Generic;
-using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HairSalon.Controllers
 {
-    public class Stylist : Controller
+    public class StylistController : Controller
     {
+        private readonly HairSalonContext _db;
+
+        public StylistController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Stylist> model = _db.Stylists.ToList();
+            return View(model);
+        }
     }
 }
