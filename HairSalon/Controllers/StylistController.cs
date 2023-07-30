ee96becbec5580a50cb9a70e9859deb825ad950e
Erik Z <molagg92@gmail.com>
2023-07-29 17:55:38 -0700

    edit and delete funciontality works

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -28,11 +28,41 @@ namespace HairSalon.Controllers
 		[HttpPost]
 		public ActionResult Create(Stylist stylist)
 		{
-			// Plural Strylists might be an issue down the road
-				_db.Stylists.Add(stylist);
-				_db.SaveChanges();
-				return RedirectToAction("Index");
+			_db.Stylists.Add(stylist);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
 		}
-			
+		public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylists
+																	.Include(stylist => stylist.Clients)
+																	.FirstOrDefault(stylist => stylist.StylistId == id);
+		return View(thisStylist);
+    }
+		public ActionResult Edit(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+		[HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Stylists.Update(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+		public ActionResult Delete(int id)
+    {
+		Stylist thisStylist = _db.Stylists.FirstOrDefault(Stylist => Stylist.StylistId == id);
+		return View(thisStylist);
+    }
+		    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(Stylist => Stylist.StylistId == id);
+      _db.Stylists.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 	}
 }
